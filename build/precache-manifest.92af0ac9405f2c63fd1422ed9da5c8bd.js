cd454432fb68ab139d4102e535e1de0736cc9b7b
sahil1693 <sahil171693.cse@chitkara.edu.in>
2020-06-24 10:31:31 +0530

    build

---
+++ build/precache-manifest.92af0ac9405f2c63fd1422ed9da5c8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1ebb164a00b8b3adf53ce2a917523c",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "80c678eb03ee6d0207ab",
+    "url": "/TicTacToe/static/css/main.86470b7f.chunk.css"
+  },
+  {
+    "revision": "b4d079368533cb9dcbfe",
+    "url": "/TicTacToe/static/js/2.c9b1944a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.c9b1944a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c678eb03ee6d0207ab",
+    "url": "/TicTacToe/static/js/main.72e6f775.chunk.js"
+  },
+  {
+    "revision": "54af3b5cfa1a5ca204c1",
+    "url": "/TicTacToe/static/js/runtime-main.794d048a.js"
+  }
+]);
